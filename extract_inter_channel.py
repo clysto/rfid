e636e293c166ab54e3e7131abd7063c0249af4c8
毛亚琛 <maoyachen55@gmail.com>
2024-05-13 21:29:25 +0800

    dbscan

--- extract_inter_channel.py
+++ extract_inter_channel.py
@@ -1,9 +1,10 @@
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
+from scipy import signal
 
 
-def extract_rn16_frames(sig, n_max_gap, n_t1, n_rn16, frame_cv_threshoud=0.04):
+def extract_rn16_frames(sig, n_max_gap, n_t1, n_rn16):
     """
     Extracts RN16 frames from a given signal.
 
@@ -12,7 +13,6 @@ def extract_rn16_frames(sig, n_max_gap, n_t1, n_rn16, frame_cv_threshoud=0.04):
     - n_max_gap (int): Maximum number of low-level continuous samples of the reader command signal.
     - n_t1 (int): Number of samples before the start of each frame.
     - n_rn16 (int): Number of RN16 samples in each frame.
-    - frame_cv_threshoud (float): Threshold for coefficient of variation (CV) to filter frames.
 
     Returns:
     - frames (numpy.ndarray): Extracted RN16 frames.
@@ -25,90 +25,69 @@ def extract_rn16_frames(sig, n_max_gap, n_t1, n_rn16, frame_cv_threshoud=0.04):
     for start, end in zip(bs_start, bs_end):
         if start >= len(sig) or end >= len(sig):
             continue
-        pkt = sig[start:end]
+        dc_est = np.mean(sig[start - 200 : start])
+        pkt = sig[start:end] - dc_est
         frames.append(pkt)
     frames = np.array(frames)
-    # cv = np.std(np.abs(frames), axis=-1) / np.mean(np.abs(frames), axis=-1)
-    # idx = np.argwhere(cv >= frame_cv_threshoud).reshape(-1)
-    # frames = frames[idx, :]
     return frames
 
 
-def symbol_samples(frame):
-    frame_diff = np.abs(np.diff(np.abs(frame)))
-    q1 = np.percentile(frame_diff, 25)
-    q3 = np.percentile(frame_diff, 75)
-    iqr = q3 - q1
-    lower_bound = q1 - 1.5 * iqr
-    upper_bound = q3 + 1.5 * iqr
-    condition = (frame_diff >= lower_bound) & (frame_diff <= upper_bound)
-    return frame[np.argwhere(condition).reshape(-1)]
+def frame_sync(frame):
+    preamble = np.repeat([1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1], 25) + 0j
+    corr = np.abs(signal.correlate(frame[: len(preamble) + 100], preamble, "same"))
+    frame_start = np.argmax(corr) - 6 * 25
+    frame = frame[frame_start:]
+    h_est = np.mean(frame[np.argwhere(preamble == 1)])
+    return frame, h_est
 
 
-def cluster_samples(samples: np.ndarray):
-    data = np.array([samples.real, samples.imag]).T
-    kmeans = KMeans(n_clusters=4)
-    kmeans.fit(data)
-    labels = kmeans.labels_
-    clustering_centers = kmeans.cluster_centers_
-    sorted_centers = np.argsort(np.linalg.norm(clustering_centers, axis=-1))
-    ll_label = sorted_centers[0]
-    lh_label = sorted_centers[1]
-    hl_label = sorted_centers[2]
-    hh_label = sorted_centers[3]
-    data_ll = data[labels == ll_label]
-    data_lh = data[labels == lh_label]
-    data_hl = data[labels == hl_label]
-    data_hh = data[labels == hh_label]
-    return {
-        "ll_center": clustering_centers[ll_label][0]
-        + clustering_centers[ll_label][1] * 1j,
-        "lh_center": clustering_centers[lh_label][0]
-        + clustering_centers[lh_label][1] * 1j,
-        "hl_center": clustering_centers[hl_label][0]
-        + clustering_centers[hl_label][1] * 1j,
-        "hh_center": clustering_centers[hh_label][0]
-        + clustering_centers[hh_label][1] * 1j,
-        "ll": data_ll[:, 0] + data_ll[:, 1] * 1j,
-        "lh": data_lh[:, 0] + data_lh[:, 1] * 1j,
-        "hl": data_hl[:, 0] + data_hl[:, 1] * 1j,
-        "hh": data_hh[:, 0] + data_hh[:, 1] * 1j,
-    }
+def cluster_frame(frame: np.ndarray):
+    dbscan = DBSCAN(eps=0.02, min_samples=50)
+    centers = np.full(4, np.nan, dtype=complex)
+    clusters = dbscan.fit_predict(np.array([frame.real, frame.imag]).T)
+    if np.max(clusters) != 3:
+        raise Exception("Expected 4 clusters, found {}".format(np.max(clusters) + 1))
+    for i in range(4):
+        centers[i] = np.mean(frame[clusters == i])
+    return clusters, centers
 
 
-def plot_result(result, expected_center):
-    plt.figure()
-    # plt.xlim(-1, 1)
-    # plt.ylim(-1, 1)
-    plt.scatter(result["ll"].real, result["ll"].imag, marker=".")
-    plt.scatter(result["lh"].real, result["lh"].imag, marker=".")
-    plt.scatter(result["hl"].real, result["hl"].imag, marker=".")
-    plt.scatter(result["hh"].real, result["hh"].imag, marker=".")
-    plt.scatter(expected_center.real, expected_center.imag, marker="x", c="b")
-    plt.scatter(result["hh_center"].real, result["hh_center"].imag, marker="x", c="b")
-    plt.show()
+def calc_inter_channel(centers):
+    centers_sorted = centers[np.argsort(np.abs(centers))]
+    inter_channel = (
+        centers_sorted[1]
+        - centers_sorted[0]
+        + centers_sorted[2]
+        - centers_sorted[0]
+        - centers_sorted[3]
+    )
+    return inter_channel
 
 
 if __name__ == "__main__":
     sig = np.fromfile("rx.cf32", dtype=np.complex64)
-    rn16_frames = extract_rn16_frames(
-        sig, n_max_gap=440, n_t1=470, n_rn16=1250, frame_cv_threshoud=0.005
-    )
-    inter_channels = []
-    samples = symbol_samples(rn16_frames[:8].reshape(-1))
-    result = cluster_samples(samples)
-    bs_1 = result["lh_center"] - result["ll_center"]
-    bs_2 = result["hl_center"] - result["ll_center"]
-    expected_center = (bs_1 + bs_2) + result["ll_center"]
-    inter_channels.append(expected_center - result["hh_center"])
-    print("")
-    print("  mag:", np.abs(expected_center - result["hh_center"]))
-    print("phase:", np.angle(expected_center - result["hh_center"]))
-    plot_result(result, expected_center)
-    # fig, ax1 = plt.subplots()
-    # ax1.plot(np.abs(inter_channels), color="C0")
-    # ax1.set_ylabel("Magnitude", color="C0")
-    # ax2 = ax1.twinx()
-    # ax2.plot(np.unwrap(np.angle(inter_channels)), color="C1")
-    # ax2.set_ylabel("Phase", color="C1")
-    # plt.show()
+    rn16_frames = extract_rn16_frames(sig, n_max_gap=440, n_t1=470, n_rn16=1250)
+
+    for frame in rn16_frames:
+        frame, h_est = frame_sync(frame)
+        frame /= h_est
+        try:
+            clusters, centers = cluster_frame(frame)
+        except Exception as e:
+            print(e)
+            continue
+        centers_sorted = centers[np.argsort(np.abs(centers))]
+        symbols = frame[clusters >= 0]
+        inter_channel = calc_inter_channel(centers)
+        center_expected = centers_sorted[3] + inter_channel
+        # print(np.abs(inter_channel * np.abs(h_est)))
+        print(np.angle(inter_channel))
+
+    plt.figure()
+    plt.xlim(-1.2, 1.2)
+    plt.ylim(-1.2, 1.2)
+    plt.gca().set_aspect("equal", adjustable="box")
+    plt.scatter(symbols.real, symbols.imag, color="gray")
+    plt.scatter(centers.real, centers.imag, c="red")
+    plt.scatter(center_expected.real, center_expected.imag, c="blue")
+    plt.show()
--- rfid_query.py
+++ rfid_query.py
@@ -27,8 +27,14 @@ import time
 
 class rfid_query(gr.top_block):
 
-    def __init__(self):
+    def __init__(self, rx_gain=0, tx_gain=80):
         gr.top_block.__init__(self, "Query RFID", catch_exceptions=True)
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.rx_gain = rx_gain
+        self.tx_gain = tx_gain
 
         ##################################################
         # Variables
@@ -53,7 +59,7 @@ class rfid_query(gr.top_block):
 
         self.uhd_usrp_source_0.set_center_freq(910e6, 0)
         self.uhd_usrp_source_0.set_antenna("TX/RX", 0)
-        self.uhd_usrp_source_0.set_gain(0, 0)
+        self.uhd_usrp_source_0.set_gain(rx_gain, 0)
         self.uhd_usrp_source_0.set_auto_dc_offset(False, 0)
         self.uhd_usrp_source_0.set_auto_iq_balance(False, 0)
         self.uhd_usrp_sink_0 = uhd.usrp_sink(
@@ -70,9 +76,10 @@ class rfid_query(gr.top_block):
 
         self.uhd_usrp_sink_0.set_center_freq(910e6, 0)
         self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
-        self.uhd_usrp_sink_0.set_gain(80, 0)
-        self.blocks_head_0_0 = blocks.head(gr.sizeof_gr_complex*1, 200000)
-        self.blocks_head_0 = blocks.head(gr.sizeof_gr_complex*1, 102400)
+        self.uhd_usrp_sink_0.set_gain(tx_gain, 0)
+        self.blocks_skiphead_0 = blocks.skiphead(gr.sizeof_gr_complex*1, (int(samp_rate * 0.003)))
+        self.blocks_head_0_0 = blocks.head(gr.sizeof_gr_complex*1, (int(samp_rate * 0.06)))
+        self.blocks_head_0 = blocks.head(gr.sizeof_gr_complex*1, (int(samp_rate * 0.05)))
         self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
         self.blocks_file_source_0 = blocks.file_source(gr.sizeof_float*1, 'reader.f32', True, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
@@ -87,22 +94,51 @@ class rfid_query(gr.top_block):
         self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_head_0_0, 0))
         self.connect((self.blocks_head_0, 0), (self.blocks_file_sink_0, 0))
         self.connect((self.blocks_head_0_0, 0), (self.uhd_usrp_sink_0, 0))
-        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_head_0, 0))
+        self.connect((self.blocks_skiphead_0, 0), (self.blocks_head_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_skiphead_0, 0))
 
+
+    def get_rx_gain(self):
+        return self.rx_gain
+
+    def set_rx_gain(self, rx_gain):
+        self.rx_gain = rx_gain
+        self.uhd_usrp_source_0.set_gain(self.rx_gain, 0)
+
+    def get_tx_gain(self):
+        return self.tx_gain
+
+    def set_tx_gain(self, tx_gain):
+        self.tx_gain = tx_gain
+        self.uhd_usrp_sink_0.set_gain(self.tx_gain, 0)
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.blocks_head_0.set_length((int(self.samp_rate * 0.05)))
         self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
         self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+        self.blocks_head_0_0.set_length((int(self.samp_rate * 0.06)))
 
 
+
+def argument_parser():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--rx-gain", dest="rx_gain", type=intx, default=0,
+        help="Set Rx Gain [default=%(default)r]")
+    parser.add_argument(
+        "--tx-gain", dest="tx_gain", type=intx, default=80,
+        help="Set Tx Gain [default=%(default)r]")
+    return parser
 
 
 def main(top_block_cls=rfid_query, options=None):
-    tb = top_block_cls()
+    if options is None:
+        options = argument_parser().parse_args()
+    tb = top_block_cls(rx_gain=options.rx_gain, tx_gain=options.tx_gain)
 
     def sig_handler(sig=None, frame=None):
         tb.stop()
